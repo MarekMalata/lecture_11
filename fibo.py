4ccee91d734140a4579aa5bf4d5026e3dff5cb3f
MarekMalata <marekmalata@seznam.cz>
2022-05-03 17:15:56 +0200

    prvni commit

--- fibo.py
+++ fibo.py
@@ -1,6 +1,22 @@
+def recursive_nth_fibo(cislo_prvku):
+    if cislo_prvku == 0:
+        return 0
+    elif cislo_prvku == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(cislo_prvku - 1) + recursive_nth_fibo(cislo_prvku-2)
+
+
+
+
+
+
+
+
 
 def main():
-    pass
+    cislo_prvku = int(input("Zadej čislo pozice hledaného prvku: "))
+    print(recursive_nth_fibo(cislo_prvku))
 
 
 if __name__ == "__main__":
