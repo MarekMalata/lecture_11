12f4d9ad01c93b6e3aa730a9769a17beb57abaf6
MarekMalata <marekmalata@seznam.cz>
2022-05-03 18:26:33 +0200

    treti commit

--- flood_fill.py
+++ flood_fill.py
@@ -1,5 +1,27 @@
 import matplotlib.pyplot as plt
 
+def flood_fill(obraz, x, y):
+    if x < 0 or x >= obraz.shape[1] or y < 0 or y >= obraz.shape[0]:
+        return obraz
+    if obraz[y,x] == 2 or obraz[y,x] == 0:
+        return obraz
+
+    obraz[y,x] = 2
+    obraz = flood_fill(obraz, x+1, y)
+    obraz = flood_fill(obraz, x-1, y)
+    obraz = flood_fill(obraz, x, y+1)
+    obraz = flood_fill(obraz, x, y-1)
+
+    plt.imshow(obraz, cmap="gray")
+    plt.show(block=False)
+    plt.pause(0.01)
+    plt.clf()
+
+    return obraz
+
+
+
+
 
 def main():
     img = plt.imread("files/img0.png")[:, :, 0]
@@ -13,6 +35,9 @@ def main():
     plt.pause(5)
     plt.clf()
 
+    flood_fill(img, 0, 0)
+
+   # print(flood_fill(img, 0, 0))
 
 if __name__ == "__main__":
     main()
